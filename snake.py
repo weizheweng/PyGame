46ece9d52324aa0094409beedb29eb959c509d4f
wei-zhe <weizheweng@google.com>
2024-11-19 22:38:53 +0800

    generate fruits with different sizes and colors

--- snake.py
+++ snake.py
@@ -34,6 +34,7 @@ class Color:
         self.white = (255, 255, 255)
         self.red = (229, 46, 8)
         self.darkRed = (157, 31, 6)
+        self.blue = (0, 0, 255)
         self.green = (64, 201, 73)
         self.darkGreen = (36, 127, 42)
 
@@ -65,120 +66,179 @@ class Snake:
 
 
 class Fruit:
-    def __init__(self, game):
+    def __init__(self, game, snake, size=1):
         self.game = game
-        self.pos = [0, 0]
+        self.snake = snake  # 這裡接收並儲存snake物件
+        self.size = size  # 水果的大小
+        self.pos = []
         self.spawn()
 
     def spawn(self):
-        self.pos = [
-            random.randrange(0, self.game.canvas_x, self.game.unit),
-            random.randrange(0, self.game.canvas_y, self.game.unit),
-        ]
+        # 確保水果不與蛇的身體重疊，且不會超過畫布的邊界
+        valid_position = False
+        while not valid_position:
+            base_x = random.randrange(0, self.game.canvas_x, self.game.unit)  # 隨機生成水果的起始位置
+            base_y = random.randrange(0, self.game.canvas_y, self.game.unit)
+            
+            # 根據水果大小來確保位置不會超過邊界
+            if self.size == 1:
+                self.pos = [[base_x, base_y]]  # 預設水果為一個unit
+            elif self.size == 2:
+                # 水平或垂直的兩個unit
+                if random.choice([True, False]):  # 隨機決定是水平方向還是垂直方向
+                    # 確保水平水果不會超過邊界
+                    if base_x + self.game.unit < self.game.canvas_x:
+                        self.pos = [[base_x, base_y], [base_x + self.game.unit, base_y]]
+                    else:
+                        continue  # 超過邊界就重新生成
+                else:
+                    # 確保垂直水果不會超過邊界
+                    if base_y + self.game.unit < self.game.canvas_y:
+                        self.pos = [[base_x, base_y], [base_x, base_y + self.game.unit]]
+                    else:
+                        continue  # 超過邊界就重新生成
+            elif self.size == 4:
+                # 四個unit組成的正方形
+                if (base_x + self.game.unit < self.game.canvas_x and 
+                    base_y + self.game.unit < self.game.canvas_y):
+                    self.pos = [
+                        [base_x, base_y],
+                        [base_x + self.game.unit, base_y],
+                        [base_x, base_y + self.game.unit],
+                        [base_x + self.game.unit, base_y + self.game.unit],
+                    ]
+                else:
+                    continue  # 超過邊界就重新生成
+
+            # 檢查水果是否與蛇身體重疊
+            valid_position = True
+            for body in self.snake.body:  # 使用snake物件中的body
+                if any(pos == body for pos in self.pos):  # 檢查水果的每個位置是否與蛇身體重疊
+                    valid_position = False  # 如果水果與蛇身體重疊，標記為無效位置，並重新生成
+                    break
+
+
 
 
 def main():
-    game = Game()
+    game = Game()  # 創建遊戲對象
+    color = Color()  # 創建顏色對象
+    score = 0  # 初始化分數
+    snake = Snake(game, color)  # 創建蛇對象
+    fruits = [
+        Fruit(game, snake, size=1),  # 一個unit的水果
+        Fruit(game, snake, size=1),  
+        Fruit(game, snake, size=1),  
+        Fruit(game, snake, size=2),  # 兩個水平或垂直unit的水果
+        Fruit(game, snake, size=2),  
+        Fruit(game, snake, size=4),  # 四個unit的正方形水果
+    ]
+
     while True:
-        score = 0
-        color = Color()
-        snake = Snake(game, color)
-        fruit = Fruit(game)
+        start_time = pygame.time.get_ticks()  # 記錄時間，用於控制遊戲速度
+        while pygame.time.get_ticks() - start_time < 1000 // game.game_speed:
+            pygame.event.pump()
 
-        while True:
-            start_time = pygame.time.get_ticks()
-            while pygame.time.get_ticks() - start_time < 1000 // game.game_speed:
-                pygame.event.pump()
-
-            # 偵測鍵盤事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key in [pygame.K_d, pygame.K_RIGHT]:
-                        snake.new_direction = "RIGHT"
-                    elif event.key in [pygame.K_a, pygame.K_LEFT]:
-                        snake.new_direction = "LEFT"
-                    elif event.key in [pygame.K_s, pygame.K_DOWN]:
-                        snake.new_direction = "DOWN"
-                    elif event.key in [pygame.K_w, pygame.K_UP]:
-                        snake.new_direction = "UP"
+        # 偵測鍵盤事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key in [pygame.K_d, pygame.K_RIGHT]:
+                    snake.new_direction = "RIGHT"
+                elif event.key in [pygame.K_a, pygame.K_LEFT]:
+                    snake.new_direction = "LEFT"
+                elif event.key in [pygame.K_s, pygame.K_DOWN]:
+                    snake.new_direction = "DOWN"
+                elif event.key in [pygame.K_w, pygame.K_UP]:
+                    snake.new_direction = "UP"
                         
-            # 判斷轉彎方向是否合理
-            if snake.new_direction == "RIGHT" and snake.direction != "LEFT":
-                snake.direction = "RIGHT"
-            elif snake.new_direction == "LEFT" and snake.direction != "RIGHT":
-                snake.direction = "LEFT"
-            elif snake.new_direction == "DOWN" and snake.direction != "UP":
-                snake.direction = "DOWN"
-            elif snake.new_direction == "UP" and snake.direction != "DOWN":
-                snake.direction = "UP"
+        # 判斷轉彎方向是否合理
+        if snake.new_direction == "RIGHT" and snake.direction != "LEFT":
+            snake.direction = "RIGHT"
+        elif snake.new_direction == "LEFT" and snake.direction != "RIGHT":
+            snake.direction = "LEFT"
+        elif snake.new_direction == "DOWN" and snake.direction != "UP":
+            snake.direction = "DOWN"
+        elif snake.new_direction == "UP" and snake.direction != "DOWN":
+            snake.direction = "UP"
 
-            if snake.direction == "RIGHT":
-                snake.head[0] += game.unit
-            elif snake.direction == "LEFT":
-                snake.head[0] -= game.unit
-            elif snake.direction == "DOWN":
-                snake.head[1] += game.unit
-            elif snake.direction == "UP":
-                snake.head[1] -= game.unit
+        if snake.direction == "RIGHT":
+            snake.head[0] += game.unit
+        elif snake.direction == "LEFT":
+            snake.head[0] -= game.unit
+        elif snake.direction == "DOWN":
+            snake.head[1] += game.unit
+        elif snake.direction == "UP":
+            snake.head[1] -= game.unit
 
-            snake.body.insert(0, list(snake.head))
+        snake.body.insert(0, list(snake.head))
 
-            if snake.head == fruit.pos:
-                game.fruit_sfx.play()
-                fruit.spawn()
-                score += 1
-            else:
-                snake.body.pop()
-                
-            # 碰撞判定
-            if not (0 <= snake.head[0] < game.canvas_x):
-                break
-            if not (0 <= snake.head[1] < game.canvas_y):
-                break
-            if snake.head in snake.body[1:]:
-                break
+        # 判斷蛇是否吃到水果
+        if any(snake.head == pos for fruit in fruits for pos in fruit.pos):
+            game.fruit_sfx.play()
+            for fruit in fruits:
+                if snake.head in fruit.pos:  # 如果蛇的頭部位置碰到水果的任何位置
+                    fruit.spawn()  # 生成新的水果
+                    score += 1  # 分數加1
+        else:
+            snake.body.pop()
 
-            game.canvas.blit(game.background, (0, 0))
-            
-            pygame.draw.rect(game.canvas, color.grey, (snake.head[0], snake.head[1], game.unit + 2, game.unit + 2))
-            for body in snake.body[1:]:
-                pygame.draw.rect(game.canvas, color.grey, (body[0], body[1], game.unit + 2, game.unit + 2))
-                
-            pygame.draw.rect(game.canvas, snake.color1, (snake.head[0] - 1, snake.head[1] - 1, game.unit + 2, game.unit + 2))
-            pygame.draw.rect(game.canvas, snake.color2, (snake.head[0] - 1, snake.head[1] - 1, game.unit + 2, game.unit + 2), 2)
+        # 判斷是否碰撞牆壁或自己
+        if not (0 <= snake.head[0] < game.canvas_x):
+            break
+        if not (0 <= snake.head[1] < game.canvas_y):
+            break
+        if snake.head in snake.body[1:]:
+            break
 
-            for body in snake.body[1:]:
-                pygame.draw.rect(game.canvas, snake.color1, (body[0], body[1], game.unit, game.unit))
-                pygame.draw.rect(game.canvas, snake.color2, (body[0], body[1], game.unit, game.unit), 2)
+        game.canvas.blit(game.background, (0, 0))
 
-            game.canvas.blit(game.face, snake.head)
+        # 畫蛇
+        pygame.draw.rect(game.canvas, color.grey, (snake.head[0], snake.head[1], game.unit + 2, game.unit + 2))
+        for body in snake.body[1:]:
+            pygame.draw.rect(game.canvas, color.grey, (body[0], body[1], game.unit + 2, game.unit + 2))
 
-            pygame.draw.rect(game.canvas, color.grey, (fruit.pos[0] + 3, fruit.pos[1] + 3, game.unit - 4, game.unit - 4), 0, 3)
-            pygame.draw.rect(game.canvas, color.red, (fruit.pos[0] + 2, fruit.pos[1] + 2, game.unit - 4, game.unit - 4), 0, 3)
-            pygame.draw.rect(game.canvas, color.darkRed, (fruit.pos[0] + 2, fruit.pos[1] + 2, game.unit - 4, game.unit - 4), 2, 3)
+        pygame.draw.rect(game.canvas, snake.color1, (snake.head[0] - 1, snake.head[1] - 1, game.unit + 2, game.unit + 2))
+        pygame.draw.rect(game.canvas, snake.color2, (snake.head[0] - 1, snake.head[1] - 1, game.unit + 2, game.unit + 2), 2)
 
-            game.window.blit(game.border, (0, 0))
-            game.window.blit(game.canvas, (30, 120))
-            Text(game, str(score), 45, color.white, "impact").midleft(90, 45)
+        for body in snake.body[1:]:
+            pygame.draw.rect(game.canvas, snake.color1, (body[0], body[1], game.unit, game.unit))
+            pygame.draw.rect(game.canvas, snake.color2, (body[0], body[1], game.unit, game.unit), 2)
 
-            pygame.display.update()
+        game.canvas.blit(game.face, snake.head)
 
-        game.game_over_sfx.play()
-        game.window.blit(game.game_over, (0, 0))
+        # 畫水果
+        for fruit in fruits:
+            for pos in fruit.pos:
+                if fruit.size == 1:
+                    temp_color = color.red  # 紅色
+                elif fruit.size == 2:
+                    temp_color = color.blue  # 藍色
+                elif fruit.size == 4:
+                    temp_color = color.grey  # 灰色
+                pygame.draw.rect(game.canvas, temp_color, (pos[0], pos[1], game.unit - 4, game.unit - 4), 0, 3)
+
+        game.window.blit(game.border, (0, 0))
+        game.window.blit(game.canvas, (30, 120))
+        Text(game, str(score), 45, color.white, "impact").midleft(90, 45)
+
         pygame.display.update()
 
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        running = False
+    game.game_over_sfx.play()
+    game.window.blit(game.game_over, (0, 0))
+    pygame.display.update()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    running = False
 
 
 if __name__ == "__main__":
